a8476ea3ced855bbbaac04f0d6df16253e490154
corjqnrl <coqnrl@ewha.ac.kr>
2025-09-18 14:01:43 +0900

    feat: 세미나 주차별 폴더 및 PR 템플릿 추가

--- week02/dist/practice1_3.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function forEach(arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i]);
-    }
-}
--- week02/src/practice1_3.ts
+++
@@ -1,5 +0,0 @@
-function forEach<T> (arr: T[], callback: (item: T) => void) {
-  for (let i = 0; i < arr.length; i++) {
-    callback(arr[i]);
-  }
-}
